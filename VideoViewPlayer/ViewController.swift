4f69aa2516a636ee6b70a32cdabb410fa1af5281
Talha Ahmed <talha180295@gmail.com>
2019-10-02 23:17:31 +0500

    Initial Commit

---
+++ VideoViewPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoViewPlayer
+//
+//  Created by talha on 02/10/2019.
+//  Copyright © 2019 talha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

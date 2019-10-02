6d0d20edbc52f45683206822c7931e4f3da25783
Talha Ahmed <talha180295@gmail.com>
2019-10-03 00:10:28 +0500

    Video View Implemented successfully

---
+++ VideoViewPlayer/VideoViewPlayer.swift
@@ -0,0 +1,75 @@
+//
+//  VideoViewPlayer.swift
+//  VideoViewPlayer
+//
+//  Created by talha on 02/10/2019.
+//  Copyright © 2019 talha. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class VideoViewPlayer: UIView {
+
+    
+    
+    var playerLayer: AVPlayerLayer?
+    var player: AVPlayer?
+    var isLoop: Bool = false
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        // The Main Stuff
+    }
+    
+        
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+    
+    func configure(videoURL: String) {
+        
+        print("configure")
+        player = AVPlayer(url: URL(fileURLWithPath: videoURL))
+        playerLayer = AVPlayerLayer(player: player)
+        playerLayer?.frame = bounds
+        playerLayer?.videoGravity = AVLayerVideoGravity.resize
+        if let playerLayer = self.playerLayer {
+            layer.addSublayer(playerLayer)
+        }
+        NotificationCenter.default.addObserver(self, selector: #selector(reachTheEndOfTheVideo(_:)), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: self.player?.currentItem)
+        
+    }
+    
+    func play() {
+        if player?.timeControlStatus != AVPlayer.TimeControlStatus.playing {
+            player?.play()
+        }
+    }
+    
+    func pause() {
+        player?.pause()
+        
+    }
+    
+    func stop() {
+        player?.pause()
+        player?.seek(to: CMTime.zero)
+        
+        playerLayer?.removeFromSuperlayer()
+    }
+    
+    @objc func reachTheEndOfTheVideo(_ notification: Notification) {
+        if isLoop {
+            player?.pause()
+            player?.seek(to: CMTime.zero)
+            player?.play()
+        }
+    }
+
+
+}
--- VideoViewPlayer/ViewController.swift
+++ VideoViewPlayer/ViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var videoView: VideoViewPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+
+        //var url = URL.init(fileURLWithPath: "https://www.youtube.com/watch?v=SeY7qcK8d-s&list=RDqLv4msFH35Y&index=8")
+        
+        guard let path = Bundle.main.path(forResource: "demo", ofType:"mov") else {
+            debugPrint("demo.mov not found")
+            return
+        }
+        self.videoView.configure(videoURL: path)
+        self.videoView.isLoop = true
+        self.videoView.play()    }
+    
 
 
 }
